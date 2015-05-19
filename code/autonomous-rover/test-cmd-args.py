ec206cb558fd7599913cdc8cf85879552f1d2b79
Thomas Enevoldsen <thomasthuesen@hotmail.com>
2015-05-19 16:26:41 +0200

    Did some restructuring

--- code/autonomous-rover/test-cmd-args.py
+++ code/autonomous-rover/test-cmd-args.py
@@ -5,7 +5,11 @@ parser.add_argument("-t","--turning",help="Add the turning delay")
 args = parser.parse_args()
 print("Hello!")
 print("Here are your args: " + str(args.distance) + " and " + str(args.turning))
-dist = args.distance
+if args.distance:
+    dist = args.distance
+else:
+    dist = 2
+    
 turn = args.turning
 
 print(int(turn) + int(dist))
