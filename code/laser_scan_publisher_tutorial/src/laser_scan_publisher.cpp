6b82a05bdefe01343f5f322fbda8818f40330b93
Emil Mar Einarsson <emilmare@hotmail.com>
2015-05-20 13:57:52 +0200

    Update laser_scan_publisher.cpp

--- code/laser_scan_publisher_tutorial/src/laser_scan_publisher.cpp
+++ code/laser_scan_publisher_tutorial/src/laser_scan_publisher.cpp
@@ -37,12 +37,43 @@
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
 #include <iostream>
+#include <wiringPi.h>
+#include <stdlib.h>
 
 extern "C" int lidar_init(bool);
 extern "C" int lidar_read(int);
 
+#define PIN16 23
+#define PIN18 24
+
+//PIN16 is the pin #16 on the pi diagram
+// use for setting up the motorcontroller
+
+//PIN18 is the pin #18 on the pi diagram
+// use for controlling the steps 
+
+void setUpPins(){
+    system("gpio export 23 out");
+    system("gpio export 24 out");
+    system("gpio -g write 23 1");
+/*    wiringPiSetupGpio();
+    pinMode(PIN16,OUTPUT);
+    pinMode(PIN18,OUTPUT);
+    digitalWrite(PIN16,HIGH);*/
+}
+
+void step(int steps){
+    for(int i = 0; i<steps; i++){
+        system("/home/ubuntu/gpioThing.sh");
+/*        digitalWrite(PIN18,HIGH);
+        digitalWrite(PIN18,LOW);*/
+    }
+}
+
+
 int main(int argc, char** argv){
     int fd = lidar_init(false);//set to true for printouts
+    setUpPins();
     // If init failed
     if (fd == -1) {
         printf("initialization error\n");
@@ -53,19 +84,22 @@ int main(int argc, char** argv){
     ros::NodeHandle n;
     ros::Publisher scan_pub = n.advertise<sensor_msgs::LaserScan>("scan", 10);
 
-    unsigned int num_readings = 200; //number of readings in one packet.
-    double laser_frequency = 0.5/*MIGHT CHANGE*/; //how many spins in one second
-    double ranges[num_readings];
+    const unsigned int num_readings = 200; //number of readings in one packet.
+    const unsigned int num_steps = 1;
+    const double laser_frequency = 0.5/*MIGHT CHANGE*/; //how many spins in one second
+//    double ranges[num_readings];
 
+    //ros::Rate fullScan(0.1/*MIGHT CHANGE*/);
+    //ros::Rate singleStep(0.25/*MIGHT CHANGE*/);
     while(n.ok()){
         ros::Time scan_time = ros::Time::now();
-
+        
         //sets up the relevant info about the laser
         sensor_msgs::LaserScan scan;
         scan.header.frame_id = "laser";
         scan.angle_min = -3.14;//angles in radians
         scan.angle_max = 3.14;
-        scan.angle_increment = 6.28 / 200;
+        scan.angle_increment = 6.28 / (200/num_steps);
         scan.time_increment = (1 / laser_frequency) / (num_readings); //time between measurements in seconds
         scan.range_min = 0.1;//the distance range. in meters
         scan.range_max = 40.0;//set to 40 or less for our laser
@@ -75,9 +109,13 @@ int main(int argc, char** argv){
         //publish the data to /scan
         scan.ranges.resize(num_readings);
         for(unsigned int i = 0; i < num_readings; ++i){
+            step(num_steps);
             scan.ranges[i] = lidar_read(fd)/100.0;
+//            std::cout << scan.ranges[i] << std::endl;
+//            singleStep.sleep();
         }
 
         scan_pub.publish(scan);
+//        fullScan.sleep();
     }
 }
