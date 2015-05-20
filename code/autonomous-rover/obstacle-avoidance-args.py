f5a530ef6ec621724c666545d727b84ec90e8910
Thomas Enevoldsen <thomasthuesen@hotmail.com>
2015-05-20 13:53:15 +0200

    Changes and additions to the discussion

--- code/autonomous-rover/obstacle-avoidance-args.py
+++ code/autonomous-rover/obstacle-avoidance-args.py
@@ -2,22 +2,23 @@
 import time
 import RPi.GPIO as GPIO
 import argparse
-
+#Ability to read arguments
+parser = argparse.ArgumentParser()
 parser.add_argument("-d","--distance",help="Add the set distance (def. 50cm)")
 args = parser.parse_args()
 
+#Setting the GPIO library to use the BCM layout
 GPIO.setmode(GPIO.BCM)
 print("Program started")
-#Pins
+
+#Ultrasonic sensor pins
 TRIGGER_LEFT = 4
 ECHO_LEFT = 17
-
 TRIGGER_RIGHT = 27
 ECHO_RIGHT = 22
-
 TRIGGER_CENTER = 23
 ECHO_CENTER = 24
-
+#Motor pins
 MOTOR_A1 = 5
 MOTOR_A2 = 6
 MOTOR_B1 = 13
@@ -27,16 +28,37 @@ dist_left = 0
 dist_right = 0
 dist_center = 0
 
+#Checking for an argument
 if args.distance:
-    avoid_at = args.distance #cm
+    avoid_at = int(args.distance) #cm
 else:
-    avoid_at = 50 #cm
-    
+    avoid_at = 50 #cm (default)
+
+#How long the rover turns    
 turn_time = 0.5 #seconds
 
 print("Pin def succes")
 
-#Get distance final
+#Setting up the GPIO pins
+GPIO.setup(TRIGGER_LEFT,GPIO.OUT)
+GPIO.setup(ECHO_LEFT,GPIO.IN)      
+
+GPIO.setup(TRIGGER_RIGHT,GPIO.OUT)
+GPIO.setup(ECHO_RIGHT,GPIO.IN)
+
+GPIO.setup(TRIGGER_CENTER, GPIO.OUT)
+GPIO.setup(ECHO_CENTER, GPIO.IN)
+
+GPIO.setup(MOTOR_A1, GPIO.OUT)
+GPIO.setup(MOTOR_A2, GPIO.OUT)
+GPIO.setup(MOTOR_B1, GPIO.OUT)
+GPIO.setup(MOTOR_B2, GPIO.OUT)
+print("Pin setup success")
+
+#####################################
+#####################################
+
+#function for retrieving the distance
 def getdist(trigger, echo):
     #Sending the 10microsecond pulse nedded
     GPIO.output(trigger, True)
@@ -57,6 +79,7 @@ def getdist(trigger, echo):
 
     return distance
 
+#The functions for the movement
 def forward():
     GPIO.output(MOTOR_A1, True)
     GPIO.output(MOTOR_A2, False)
@@ -101,22 +124,6 @@ def reverse():
 #####################################
 #####################################
 
-#Setting up the GPIO pins
-GPIO.setup(TRIGGER_LEFT,GPIO.OUT)
-GPIO.setup(ECHO_LEFT,GPIO.IN)      
-
-GPIO.setup(TRIGGER_RIGHT,GPIO.OUT)
-GPIO.setup(ECHO_RIGHT,GPIO.IN)
-
-GPIO.setup(TRIGGER_CENTER, GPIO.OUT)
-GPIO.setup(ECHO_CENTER, GPIO.IN)
-
-GPIO.setup(MOTOR_A1, GPIO.OUT)
-GPIO.setup(MOTOR_A2, GPIO.OUT)
-GPIO.setup(MOTOR_B1, GPIO.OUT)
-GPIO.setup(MOTOR_B2, GPIO.OUT)
-print("Pin setup success")
-
 #Setting up the sensor (0.1second to 'load' the module)
 GPIO.output(TRIGGER_LEFT, False)
 GPIO.output(TRIGGER_RIGHT, False)
@@ -134,36 +141,40 @@ print("Inital distance complete")
 try:
     print("Navigation loop started")
     while True:
+        #The time between each cycle (mininum recommended is 60ms)
         time.sleep(0.200)
+        #Measuring the distances
         dist_left = getdist(TRIGGER_LEFT, ECHO_LEFT)
         dist_right = getdist(TRIGGER_RIGHT, ECHO_RIGHT)
         dist_center = getdist(TRIGGER_CENTER, ECHO_CENTER)
-
+        #Moves forward (As a default)
         forward()
+        print("Going forward")
 
+        #Checking the different thresholds
         if dist_left < avoid_at:
             right()
-            print("dist_left < " + str(avoid_at) + " --> Turning right")
+            print("dist_left ("+str(dist_left)+") < " + str(avoid_at) + " -> Turning right")
             time.sleep(turn_time)
-            print("Going forward")
-            forward()
         elif dist_right < avoid_at:
-            print("dist_right < " + str(avoid_at) + " --> Turning left")
             left()
+            print("dist_right ("+str(dist_right)+") < " + str(avoid_at) + " -> Turning left")
             time.sleep(turn_time)
-            forward()
         elif dist_center < avoid_at:
-            print("dist_center < " + str(avoid_at) + " --> Detecting optimal direction")
+            print("dist_center (" + str(dist_center)+ ") < " + str(avoid_at) + " --> Detecting optimal direction")
             if dist_left < dist_right:
-                print("dist_left < dist_right --> Turning right")
+                print("dist_left < dist_right -> Turning right")
                 right()
                 time.sleep(turn_time)
             else:
-                print("dist_left > dist_right --> Turning right")
+                print("dist_left > dist_right -> Turning left")
                 left()
                 time.sleep(turn_time)
 
 except KeyboardInterrupt:
+    #The code waits for a keyboardinterrupt
+    #Which ends the while loop
     pass
 
+#Cleans up the GPIO pins, to safely exit the code.
 GPIO.cleanup()
